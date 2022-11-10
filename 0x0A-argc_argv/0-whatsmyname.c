16a2856e463cd490bb6914784ee5d982d9c0a8df
vincent3767 <luckyhappyvincent@gmail.com>
2022-11-09 16:42:19 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
